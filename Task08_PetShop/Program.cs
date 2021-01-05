aed939e03d4c8bc4e759282431e8ce4a0e3e3ea1
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 20:56:12 +0200

    Add files via upload

---
+++ Task08_PetShop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_08_PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+            int allRest = int.Parse(Console.ReadLine());
+            double dogPrice = 2.50;
+            double allRestPrice = 4;
+            double suma = dogs * dogPrice + allRest * allRestPrice;
+            Console.WriteLine($"{suma} lv.");
+        }
+    }
+}
