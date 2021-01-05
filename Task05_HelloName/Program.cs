e743f52fa99d457e12950fa291ce5f122641b565
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 20:53:11 +0200

    Add files via upload

---
+++ Task05_HelloName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task05_HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
