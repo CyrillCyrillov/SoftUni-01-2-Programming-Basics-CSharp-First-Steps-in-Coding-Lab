cf3962a285e92581abd44934f63e4022c96a8902
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 20:51:51 +0200

    Add files via upload

---
+++ Task04_InchToCent/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task04_InchToCent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+            double cent = inch * 2.54;
+            Console.WriteLine($"{cent:F2}");
+        }
+    }
+}
