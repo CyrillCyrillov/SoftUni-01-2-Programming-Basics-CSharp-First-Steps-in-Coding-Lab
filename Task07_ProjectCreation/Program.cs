da7efb794d234032c49f8246d3877a4e467e7f42
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 20:55:03 +0200

    Add files via upload

---
+++ Task07_ProjectCreation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task07_ProjectCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int numberOfProjects = int.Parse(Console.ReadLine());
+            int timePerProject = 3;
+            int timeForAllProjects = numberOfProjects * timePerProject;          
+            Console.WriteLine($"The architect {name} will need {timeForAllProjects} hours to complete {numberOfProjects} project/s.");
+        }
+    }
+}
