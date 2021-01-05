f22525478181a6621d263aea0161b1d801b9c4ef
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-05 20:57:02 +0200

    Add files via upload

---
+++ Task09_YardGreening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task09_YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double yardArea = double.Parse(Console.ReadLine());
+            double price = 7.61;
+            double discont = 0.18;
+            double discontSuma = yardArea * price * discont;
+            double suma = (yardArea * price) - discontSuma;
+            Console.WriteLine($"The final price is: {suma} lv.");
+            Console.WriteLine($"The discont is: {discontSuma} lv.");
+        }
+    }
+}
